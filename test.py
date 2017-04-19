81c1e63422e56150c17cb20a74b1940a41150507
Jeffrey Whitaker <jswhit@Jeffreys-MacBook-Air.local>
2017-04-19 07:12:31 -0400

    use doctest

--- test.py
+++ test.py
@@ -1,20 +1,26 @@
-import ftype
-import numpy as np
-iarr = np.linspace(1,5,5)
-fdt = ftype.SomeDerivedType(i=-1,name='foo',iarr=iarr)
-print fdt.i
-print fdt.name
-print fdt.iarr
-fdt.i = 6
-fdt.iarr = np.ones(5)
-fdt.name = 'goober'
-print fdt.i
-print fdt.name
-print fdt.iarr
-# should get:
-# -1
-# foo
-# [1 2 3 4 5]
-# 6
-# goober
-# [1 1 1 1 1]
+def test():
+    """
+    >>> import ftype
+    >>> import numpy as np
+    >>> iarr = np.linspace(1,5,5)
+    >>> fdt = ftype.SomeDerivedType(i=-1,name='foo',iarr=iarr)
+    >>> fdt.i
+    -1
+    >>> fdt.name
+    'foo'
+    >>> fdt.iarr
+    array([1, 2, 3, 4, 5], dtype=int32)
+    >>> fdt.i = 6
+    >>> fdt.iarr = np.ones(5)
+    >>> fdt.name = 'goober'
+    >>> fdt.i
+    6
+    >>> fdt.name
+    'goober'
+    >>> fdt.iarr
+    array([1, 1, 1, 1, 1], dtype=int32)
+    """
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
